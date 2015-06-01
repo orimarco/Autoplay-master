5ea4afc21f09a873bdd7b0704f255a465bbdccfe
ori marco <marcovitch.ori@gmail.com>
2015-06-01 18:50:16 +0300

    initial commit enjoy :-)

---
+++ autoplay_backend/src/main/java/com/autoplay/backend/anotherServlet.java
@@ -0,0 +1,29 @@
+/*
+   For step-by-step instructions on connecting your Android application to this backend module,
+   see "App Engine Java Servlet Module" template documentation at
+   https://github.com/GoogleCloudPlatform/gradle-appengine-templates/tree/master/HelloWorld
+*/
+
+package com.autoplay.backend;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * A simple servlet that proxies reads and writes to its Google Cloud Storage bucket.
+ */
+@SuppressWarnings("serial")
+public class anotherServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    }
+}
